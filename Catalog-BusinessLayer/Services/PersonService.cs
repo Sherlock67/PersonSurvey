24e4d9112f63d6a16f363d778033ee0bfe91feda
sherlock67 <alammushfiqul366@gmail.com>
2022-06-14 12:58:58 +0600

    create repository for person

---
+++ Catalog-BusinessLayer/Services/PersonService.cs
@@ -0,0 +1,23 @@
+﻿using Catalog_DataAccessLayer.Interfaces;
+using Catalog_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog_BusinessLayer.Services
+{
+    public class PersonService 
+    {
+        public readonly IPerson _person;
+        public PersonService(IPerson person)
+        {
+           _person = person;
+        }
+        public async Task<Person> CreatePerson(Person person)
+        {
+            return await _person.Create(person);
+        }
+    }
+}
--- Catalog-DataAccessLayer/Repository/PersonRepository.cs
+++ Catalog-DataAccessLayer/Repository/PersonRepository.cs
@@ -1,4 +1,5 @@
-﻿using Catalog_DataAccessLayer.Interfaces;
+﻿using Catalog_DataAccessLayer.Data;
+using Catalog_DataAccessLayer.Interfaces;
 using Catalog_DataAccessLayer.Models;
 using System;
 using System.Collections.Generic;
@@ -10,26 +11,29 @@ namespace Catalog_DataAccessLayer.Repository
 {
     public class PersonRepository : IPerson
     {
-        public Task<Person> Create(Person entity)
+        public readonly ApplicationDbContext _db;
+        public PersonRepository(ApplicationDbContext db)
         {
-            throw new NotImplementedException();
+            _db = db;
         }
-
+        public async Task<Person> Create(Person entity)
+        {
+            var obj = await _db.person.AddAsync(entity);
+            _db.SaveChanges();
+            return obj.Entity;
+        }
         public void Delete(Person entity)
         {
             throw new NotImplementedException();
         }
-
         public IEnumerable<Person> GetAll()
         {
             throw new NotImplementedException();
         }
-
         public Person GetById(int id)
         {
             throw new NotImplementedException();
         }
-
         public void Update(Person entity)
         {
             throw new NotImplementedException();
